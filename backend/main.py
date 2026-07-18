073e3d98fa7d6a5c3278a4f8c402a515eccb7776
Ritesh Karki <karkiritesh11@gmail.com>
2026-07-18 12:00:46 +0530

    Initial project setup with FastAPI

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+from backend.config.config import settings
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION,
+)
+
+@app.get("/")
+def home():
+    return {
+        "message": "Welcome to Atlas Finance AI"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
